c30eebca3acb160cdaacddf817ee1074b1a80fa2
Rizki <62058876+Rizkimardianto@users.noreply.github.com>
2020-06-06 19:09:31 +0700

    Update index.php

--- tugas12/index.php
+++ tugas12/index.php
@@ -27,8 +27,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
         while ($user_data = mysqli_fetch_array($result)) {
             echo "<tr>";
             echo "<td>" . $user_data['name'] . "</td>";
-            echo "<td>" . $user_data['mobile'] . "</td>";
-            echo "<td>" . $user_data['email'] . "</td>";
+            echo "<td>" . $user_data['Judul'] . "</td>";
+            echo "<td>" . $user_data['Tanggal'] . "</td>";
             echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";
         }
         ?>
