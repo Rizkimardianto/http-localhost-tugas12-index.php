e26e4c61f3dd24e6c24484e3fa63854ab5f9114e
Rizki <62058876+Rizkimardianto@users.noreply.github.com>
2020-06-06 19:03:14 +0700

    Update edit.php

--- tugas12/edit.php
+++ tugas12/edit.php
@@ -7,11 +7,11 @@ if (isset($_POST['update'])) {
     $id = $_POST['id'];
 
     $name = $_POST['name'];
-    $mobile = $_POST['mobile'];
-    $email = $_POST['email'];
+    $mobile = $_POST['Judul'];
+    $email = $_POST['Tanggal'];
 
     // update user data
-    $result = mysqli_query($mysqli, "UPDATE users SET name='$name',email='$email',mobile='$mobile' WHERE id=$id");
+    $result = mysqli_query($mysqli, "UPDATE users SET name='$name',Judul='$Judul',Tanggal='$Tanggal' WHERE id=$id");
 
     // Redirect to homepage to display updated user in list
     header("Location: index.php");
@@ -27,8 +27,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
 
 while ($user_data = mysqli_fetch_array($result)) {
     $name = $user_data['name'];
-    $email = $user_data['email'];
-    $mobile = $user_data['mobile'];
+    $email = $user_data['Judul'];
+    $mobile = $user_data['Tanggal'];
 }
 ?>
 <html>
@@ -49,11 +49,11 @@ while ($user_data = mysqli_fetch_array($result)) {
             </tr>
             <tr>
                 <td>Email</td>
-                <td><input type="text" name="email" value=<?php echo $email; ?>></td>
+                <td><input type="text" name="Judul" value=<?php echo $Judul; ?>></td>
             </tr>
             <tr>
                 <td>Mobile</td>
-                <td><input type="text" name="mobile" value=<?php echo $mobile; ?>></td>
+                <td><input type="text" name="tanggal" value=<?php echo $Tanggal; ?>></td>
             </tr>
             <tr>
                 <td><input type="hidden" name="id" value=<?php echo $_GET['id']; ?>></td>
